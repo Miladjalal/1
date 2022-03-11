cfb571a8c23bdf7bc8ff474cb6629732cf0bfcd8
milad <miladjalali1384@gmail.com>
2022-03-11 18:16:26 +0800

    1

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿for (int i = 0; i < 4; i++)
+{
+    for (int x = 4; x > i+1; x--)
+    {
+        System.Console.Write(" ");
+    }
+    for (int j = 0; j <= (i * 2); j++)
+    {
+        Console.Write("*");
+    }
+    Console.WriteLine("");
+}
